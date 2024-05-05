73f27ec54c9aa3edce6128b0bcabd5e27a703536
Samuel Huber <samu@hubi.net>
2024-05-05 17:23:21 +0200

    assume kv return are objects directly

--- app/utils.ts
+++ app/utils.ts
@@ -6,13 +6,7 @@ export const generateMatches = async (kv: VercelKV, week: number) => {
     const matchesSet = `rsvp-week-${week}-matches`;
 
     // Get all members of the rsvp set
-    const rsvpMembers = await kv.smembers(rsvpSet);
-    console.log('generateMatches rsvpMembers', rsvpMembers, typeof(rsvpMembers));
-    console.log('generateMatches rsvpMembers[0]', rsvpMembers[0]);
-    console.log('generateMatches rsvpMembers[1]', rsvpMembers[1]);
-
-    // Convert members to an array of objects
-    const rsvpObjects: { fid: number, fname: string, matched: boolean }[] = rsvpMembers.map((member: string) => JSON.parse(member) as { fid: number, fname: string, matched: boolean });
+    const rsvpObjects: { fid: number, fname: string, matched: boolean }[] = await kv.smembers(rsvpSet);
     console.log('generateMatches rsvpObjects', rsvpObjects);
 
     // Shuffle the array of objects
